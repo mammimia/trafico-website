902331444d01dc58d333a748976e21eaa90ac96c
Muhammed Ali <muhammedaliaydin97@gmail.com>
2022-10-16 00:37:39 +0300

    Testimonials section implemented

--- components/Slider.js
+++ components/Slider.js
@@ -1,5 +1,27 @@
-const Slider = () => {
-  return <div>Slider</div>;
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+const Slider = ({ clients }) => {
+  return (
+    <Swiper slidesPerView={3} spaceBetween={20} slidesOffsetBefore={60} grabCursor className="testimonialSlider">
+      {clients.map((client, index) => {
+        const { message, image, name } = client;
+
+        return (
+          <SwiperSlide key={index}>
+            <div className="bg-white h-[360px] drop-shadow-primary rounded-[10px] px-[50px] pt-[60px] pb-[40px] flex flex-col justify-between">
+              <p className="font-ling leading-[30px]">{message}</p>
+              <div className="flex items-center gap-x-5">
+                <Image src={image} alt="" width={60} height={60} />
+                <span className="font-semibold">{name}</span>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default Slider;
