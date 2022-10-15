c5d43d062dc403923fc5e571b1cfa9f233afc3d9
Muhammed Ali <muhammedaliaydin97@gmail.com>
2022-10-15 21:17:03 +0300

    Data destructered and passed to related components

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,42 @@
 // import components
-import Hero from '../components/Hero';
-import About from '../components/About';
-import How from '../components/How';
-import Faq from '../components/Faq';
-import Testimonials from '../components/Testimonials';
-import Footer from '../components/Footer';
-import Copyright from '../components/Copyright';
+import Hero from "../components/Hero";
+import About from "../components/About";
+import How from "../components/How";
+import Faq from "../components/Faq";
+import Testimonials from "../components/Testimonials";
+import Footer from "../components/Footer";
+import Copyright from "../components/Copyright";
+import { headerData, heroData, navData, faqData, aboutData, howData, testimonialData, footerData } from "../data.json";
 
 // import data
 
-const Home = () => {
+const Home = ({ headerData, heroData, navData, faqData, aboutData, howData, testimonialData, footerData }) => {
   return (
-    <div className='overflow-hidden max-w-[1600px] mx-auto bg-page'>
-      <Hero />
-      <About />
-      <How />
-      <Faq />
-      <Testimonials />
-      <Footer />
+    <div className="overflow-hidden max-w-[1600px] mx-auto bg-page">
+      <Hero heroData={heroData} headerData={headerData} navData={navData} />
+      <About aboutData={aboutData} />
+      <How howData={howData} />
+      <Faq faqData={faqData} />
+      <Testimonials testimonialData={testimonialData} />
+      <Footer footerData={footerData} />
       <Copyright />
     </div>
   );
 };
 
-// get data.json
+export const getStaticProps = async () => {
+  return {
+    props: {
+      headerData,
+      heroData,
+      navData,
+      faqData,
+      aboutData,
+      howData,
+      testimonialData,
+      footerData,
+    },
+  };
+};
+
 export default Home;
