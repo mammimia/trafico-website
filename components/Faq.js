94cb24a2a12544f657e3ec4f673a5c94df9bc50c
Muhammed Ali <muhammedaliaydin97@gmail.com>
2022-10-16 00:19:25 +0300

    Faq section implemented

--- components/Faq.js
+++ components/Faq.js
@@ -1,5 +1,46 @@
-const Faq = () => {
-  return <div>Faq</div>;
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Accordion from "../components/Accordion";
+import { fadeInLeft, fadeInRight, staggerAccordionContainer, staggerTextContainer } from "../variants";
+
+const Faq = ({ faqData }) => {
+  const { pretitle, title, boyImg, accordions } = faqData;
+  return (
+    <section
+      className="min-h-[1150px] lg:bg-faq bg-no-repeat bg-right-top
+     lg:pt-40 lg:pb-24 mb-[60px] lg:mb-[160px]"
+    >
+      <div className="container mx-auto">
+        <motion.div
+          variants={staggerTextContainer}
+          initial="initial"
+          whileInView={"animate"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="relative flex flex-col lg:flex-row justify-center
+         lg:justify-start items-center pt-8 pb-16 lg:pt-16 lg:pb-32"
+        >
+          <motion.div variants={fadeInRight} className="lg:max-w-[45%]">
+            <h3 className="h3">{pretitle}</h3>
+            <h2 className="h2 mb-6 lg:mb-12">{title}</h2>
+          </motion.div>
+          <motion.div variants={fadeInLeft} className="lg:absolute lg:-right-16 lg:-top-16">
+            <Image src={boyImg} alt="" height={498} width={708} />
+          </motion.div>
+        </motion.div>
+        <motion.div
+          variants={staggerAccordionContainer}
+          initial="initial"
+          whileInView={"animate"}
+          viewport={{ once: false, amount: 0.4 }}
+          className="grid grid-cols-1 lg:grid-cols-2 gap-x-[20px] gap-y-[10px]"
+        >
+          {accordions.map((accordion, index) => {
+            return <Accordion key={index} accordion={accordion} />;
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Faq;
